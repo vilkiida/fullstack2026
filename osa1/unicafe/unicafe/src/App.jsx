60d01b9d387e2310b73b948deb79184e02335bf7
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2026-05-19 19:04:31 +0300

    1.7 unicafe

--- osa1/unicafe/unicafe/src/App.jsx
+++ osa1/unicafe/unicafe/src/App.jsx
@@ -5,26 +5,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
   
   const handleGoodClick = () => {
     const updatedGood = good + 1
+    const updatedAll = all + 1
     setGood(updatedGood)
+    setAll(updatedAll)
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
+    const updatedAll = all + 1
     setNeutral(updatedNeutral)
+    setAll(updatedAll)
+    
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1
+    const updatedAll = all + 1
     setBad(updatedBad)
+    setAll(updatedAll)
   }
  
   return (
     <div>
       <Feedback good={handleGoodClick} neutral={handleNeutralClick} bad={handleBadClick}/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
     </div>
   )
 }
@@ -49,12 +57,19 @@ const Feedback = (props) => (
     <Button onClick={props.bad} text='bad'/>
   </div>
 )
-const Statistics = (props) => (
+const Statistics = (props) => {
+  const average = (1 * props.good + 0 * props.neutral + -1 * props.bad)/props.all
+  const positive = props.good / props.all
+  return (
   <div>
     <Header header='statistics'/>
     <Display text='good' amount={props.good}/>
     <Display text='neutral' amount={props.neutral}/>
     <Display text='bad' amount={props.bad}/>
+    <Display text='all' amount={props.all}/>
+    <Display text='average' amount={average}/>
+    <Display text='positive' amount={positive.toString()+'%'}/>
   </div>
-)
+  )
+}
 export default App
