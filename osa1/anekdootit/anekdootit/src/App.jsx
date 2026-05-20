dd751c2331210edd50dadc4534dabef245ca6bff
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2026-05-20 19:25:35 +0300

    1.13 anekdootit

--- osa1/anekdootit/anekdootit/src/App.jsx
+++ osa1/anekdootit/anekdootit/src/App.jsx
@@ -13,13 +13,23 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  
   const selectRandom = () => {
     const random = Math.floor(Math.random() * anecdotes.length)
     setSelected(random)
   }
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  
+  const voteSelected = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button onClick={voteSelected} text='vote'/>
       <Button onClick={selectRandom} text='next anecdote'/>
     </div>
   )
